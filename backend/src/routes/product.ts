90c60f4efdf1eaa44401a2812782b228360c1097
Sasha Markov <sasha.markov.mail@gmail.com>
2026-05-31 15:28:58 +0300

    refactor: вернул авторизацию в роуты

--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -13,8 +13,8 @@ const router = Router();
 
 router.get('/', getProducts);
 router.get('/:productId', getProductById);
-router.post('/', /* authMiddleware, */ validateCreateProduct, createProduct);
-router.patch('/:productId', /* authMiddleware, */ validateUpdateProduct, updateProduct);
-router.delete('/:productId', /* authMiddleware, */ deleteProduct);
+router.post('/', authMiddleware, validateCreateProduct, createProduct);
+router.patch('/:productId', authMiddleware, validateUpdateProduct, updateProduct);
+router.delete('/:productId', authMiddleware, deleteProduct);
 
 export default router;
